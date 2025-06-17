dd973b310b69889ddab7c0d0dcccb8b693034b68
Jinyoung Jang <redpwon@gmail.com>
2025-06-17 15:33:23 +0900

    feat : 잔액 충전 실패 로그 추가

--- src/main/java/kr/hhplus/be/server/reservation/application/balance/ChargeBalanceUseCase.java
+++ src/main/java/kr/hhplus/be/server/reservation/application/balance/ChargeBalanceUseCase.java
@@ -3,8 +3,10 @@ package kr.hhplus.be.server.reservation.application.balance;
 import kr.hhplus.be.server.reservation.exception.balance.WalletNotFoundException;
 import kr.hhplus.be.server.reservation.domain.model.Balance;
 import kr.hhplus.be.server.reservation.domain.repository.BalanceRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 public class ChargeBalanceUseCase {
 
     private final BalanceRepository balanceRepository;
@@ -21,6 +23,8 @@ public class ChargeBalanceUseCase {
                 .orElseThrow(() -> new WalletNotFoundException(userId));
 
         Balance charged = target.charge(chargeAmount);
+
+        log.info("ChargeBalanceUseCase.charge :: 잔액 충전 성공 userId : {}, chargeAmount : {}", userId, chargeAmount);
 
         return balanceRepository.save(charged);
 
--- src/main/java/kr/hhplus/be/server/reservation/exception/balance/WalletNotFoundException.java
+++ src/main/java/kr/hhplus/be/server/reservation/exception/balance/WalletNotFoundException.java
@@ -1,12 +1,15 @@
 package kr.hhplus.be.server.reservation.exception.balance;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Slf4j
 @ResponseStatus(HttpStatus.NOT_FOUND)/*ControllerAdvice 없음.*/
 public class WalletNotFoundException extends RuntimeException {
 
     public WalletNotFoundException(Long walletId) {
         super("지갑을 찾을 수 없습니다. walletId = "+ walletId);
+        log.error("잔액 충전 오류 :: walletId = {}", walletId);
     }
 }
